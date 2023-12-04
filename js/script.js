7e3d89a45572aef3ab09c7666683939008ee6cfe
Giovannidubaldo <giovannidubaldo.7@gmail.com>
2023-12-04 15:22:11 +0100

    Loaded Milestone 1

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// MILESTONE 1
+let worker = [
+    {
+       'name' : 'Wayne Barnett',
+       'role' : 'Founder & CEO',
+       'image' : 'wayne-barnet.img',
+    },
+    {
+        'name' : 'Angela Caroll',
+        'role' : 'Chief Editor',
+        'image' : 'angela-caroll.img'
+    },
+    {
+        'name' : 'Walter Gordom',
+        'role' : 'Office Manager',
+        'image' : 'walter-gordom.img'
+    },
+    {
+        'name' : 'Angela Lopez',
+        'role' : 'Social Media Manager',
+        'image' : 'angela-lopez.img'
+    },
+    {
+        'name' : 'Scott Estrada',
+        'role' : 'Developer',
+        'image' : 'scott-estrada.img'
+    },
+    {
+        'name' : 'Barbara Ramos',
+        'role' : 'Graphic Designer',
+        'image' : 'barbara-ramos.img'
+    }
+]
+
+console.log(worker)
